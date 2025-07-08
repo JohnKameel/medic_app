731380318b64a79f6cbfc8ab94a9928f38871411
JohnKameel <jookameel10@gmail.com>
2025-07-08 22:35:32 +0300

    - add images & svgs folders for assets - add flutter_svg & flutter_screenutil - made a file for doc app client - add go_router package & file - add colors & text style - add empty login screen - add onboarding screen

---
+++ lib/features/onboarding/presentation/view/widgets/get_started_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:medic_app/core/routing/router_app.dart';
+import 'package:medic_app/core/theming/app_colors.dart';
+import 'package:medic_app/core/theming/app_style.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        context.push(RouterApp.login);
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(AppColors.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: MaterialStateProperty.all(const Size(double.infinity, 52)),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+      ),
+      child: Text(
+        'Get Started',
+        style: AppStyle.font16WhiteSemiBold,
+      ),
+    );
+  }
+}
